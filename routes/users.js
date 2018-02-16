b0207c3adf5d5a0ab57e81a7dcf60e39b02a4098
bombay <bomjaruwit@gmail.com>
2018-02-16 21:04:44 +0700

    request json method post and use function create and return data of schema mongo

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,17 @@
 const express = require('express');
-
-// set up router
 const router = express.Router();
+const User = require('../models/user');
+ 
 
 router.get('/users', function(req, res){
     res.send({type :'GET'});
 });
 
 router.post('/users', function(req, res){
-    res.send(req.body); 
-});
+    User.create(req.body).then(function(data){
+        res.send(data); 
+    });
+}); 
 
 router.put('/users/:id', function(req, res){
     res.send({type :'PUT'});

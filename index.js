386672b3e1290d16449a893933466c4aa74517d3
bombay <bomjaruwit@gmail.com>
2018-02-16 19:26:43 +0700

    test api get and add nodemon for automatically restart the server

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require('express');
 //set up express app
 const app = express();
 
+app.get('/api/users', function(req, res){
+    res.send({name : 'jaruwit suriyo2'});
+});
+
 //listen for requests server port
 let serverPort = 4000; 
 app.listen(process.env.port || serverPort, function(){

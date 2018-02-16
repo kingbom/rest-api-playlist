4806d7fa3e3ea3d6bc11037f7ec6492dbfe1ae5c
bombay <bomjaruwit@gmail.com>
2018-02-16 19:51:44 +0700

    change solution router and use router

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const routesUsers = require('./routes/users')
 
 //set up express app
 const app = express();
 
-//use router
-app.use(routesUsers);
+//use router of user
+app.use('/api', require('./routes/users'));
 
 //listen for requests server port
 let serverPort = 4000; 

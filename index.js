80126e14b9741818b12e65c98c4d026ca3961629
bombay <bomjaruwit@gmail.com>
2018-02-17 18:25:38 +0700

    chage config database

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const config = require("./config/database");
 
 //set up express app
 const app = express();
 
-//connect to mongodb
-mongoose.connect('mongodb://localhost/restapi');
+//set connect to mongodb
+mongoose.connect(config.database);
 mongoose.Promise = global.Promise;
+mongoose.connection.on('connected', () => console.log('connected to database :', config.database));
+mongoose.connection.on('error', () => console.log('connect to database error :', config.database));
 
 //use body parser 
 app.use(bodyParser.json());
@@ -15,13 +18,13 @@ app.use(bodyParser.json());
 //use router of user
 app.use('/api', require('./routes/users'));
 
-//error handing middleware
-app.use(function(err, req, res, next){
+//error handling middleware
+app.use((err, req, res, next) => {
     res.status(400).send({error : err.message}); 
 });
 
 //listen for requests server port
-let serverPort = 4000; 
-app.listen(process.env.port || serverPort, function(){
+let serverPort = process.env.PORT || 4000; 
+app.listen(serverPort, () => {
     console.log('application started on port :', serverPort);
 });
--- routes/users.js
+++ routes/users.js
@@ -2,34 +2,33 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
  
-router.get('/users', function(req, res, next){
+router.get('/users', (req, res, next) => {
     User.find().then(function(data){
         res.send(data); 
     });
 });
 
-router.get('/users/:id', function(req, res, next){
+router.get('/users/:id', (req, res, next) => {
     User.findById({_id : req.params.id}).then(function(data){
         checkDataToRes(res, data);
     });
 });
 
-router.post('/users', function(req, res, next){
+router.post('/users', (req, res, next) => {
     User.create(req.body).then(function(data){
         res.send(data); 
     }).catch(next);
-    
 });
  
-router.put('/users/:id', function(req, res, next){
+router.put('/users/:id', (req, res, next) => {
     User.findByIdAndUpdate({_id : req.params.id}, req.body).then(function(data){
         User.findById({_id : req.params.id}).then(function(data){
             checkDataToRes(res, data);
-        })
+        });
     });
 });
 
-router.delete('/users/:id', function(req, res, next){
+router.delete('/users/:id', (req, res, next) => {
     User.findByIdAndRemove({_id : req.params.id}).then(function(data){
         checkDataToRes(res, data);
     });
